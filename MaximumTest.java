8d3e6cc276e27fabdbf8b4f79e67a37cd7c7389d
KratiGupta <kratigupta8545@gmail.com>
2021-08-05 13:34:44 +0530

    Added welcome message to master branch

---
+++ MaximumTest.java
@@ -0,0 +1,13 @@
+package testMaximum;
+
+	public class MaximumTest{
+	     
+
+		public static void main(String args[]) {
+
+			System.out.println("Welcome to master branch of TEST MAXIMUM");
+			
+		}
+	}
+
+
